00fd5dfcd4b2bc38a330683256e65e6feb916505
Branislav <brano.obrtac@gmail.com>
2022-07-25 11:03:24 +0200

    End of Section 5

---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+interface Props {
+  openForm: () => void;
+}
+
+function NavBar({ openForm }: Props) {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "1em" }}
+          ></img>
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities"></Menu.Item>
+        <Menu.Item>
+          <Button
+            onClick={openForm}
+            positive
+            content="Create Activity-"
+          ></Button>
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+}
+
+export default NavBar;
